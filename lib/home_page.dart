7c1a0ad909df6aa1c8242627720a7bd28854c6bd
raunakpurohit <raunak.wscubetech@gmail.com>
2025-05-30 17:27:43 +0530

    prefs task completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,8 @@ class _HomePageState extends State<HomePage> {
   var nameController = TextEditingController();
   SharedPreferences? prefs;
   String name = "";
+  int count = 0;
+  Color bgColor = Colors.white;
 
   @override
   void initState(){
@@ -21,6 +23,18 @@ class _HomePageState extends State<HomePage> {
   void getName() async{
     prefs = await SharedPreferences.getInstance();
     name = prefs!.getString("name") ?? "Default";
+    count = prefs!.getInt("count") ?? 0;
+    count++;
+    prefs!.setInt("count", count);
+    if(count%3==0 && count%5==0){
+      bgColor = Colors.orange.shade200;
+    } else if(count%3==0){
+      bgColor = Colors.blue.shade200;
+    } else if(count%5==0){
+      bgColor = Colors.red.shade200;
+    } else {
+      bgColor = Colors.white;
+    }
     print(name);
     setState(() {
 
@@ -31,26 +45,44 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: bgColor,
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text("Welcome: $name"),
-            TextField(
-              controller: nameController,
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Center(
+                child: Text("$count", style: TextStyle(fontSize: 300, color: Colors.black12),),
+              ),
             ),
-            ElevatedButton(onPressed: () async{
-              ///set
-              /*SharedPreferences prefs = await SharedPreferences.getInstance();
-              */
-              prefs!.setString("name", nameController.text);
-            }, child: Text('Save'))
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Welcome: $name"),
+                TextField(
+                  controller: nameController,
+                ),
+                ElevatedButton(onPressed: () async{
+                  ///set
+                  /*SharedPreferences prefs = await SharedPreferences.getInstance();
+                  */
+                  prefs!.setString("name", nameController.text);
+                }, child: Text('Save'))
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+/// 0. Practise the above code by your own.
+/// 1. Tell the user how many times they have opened the app
+/// 2. Change the background color to blue on every 3rd time they open the app
+/// 3. Change the background color to red on every 5th time they open the app
+/// 4. Change the background color to orange on every multiple of 3 and 5 time they open the app
